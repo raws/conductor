4b75b32c77804b97f2b7e4d695519f8b170d2073
Ross Paffett <ross@rosspaffett.com>
2011-04-08 01:02:31 -0400

    Add /to <destination> command

--- src/com/raws/conductor/ConductorPlugin.java
+++ src/com/raws/conductor/ConductorPlugin.java
@@ -1,17 +1,25 @@
 package com.raws.conductor;
 
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.afforess.minecartmaniacore.utils.StringUtils;
 
 public class ConductorPlugin extends JavaPlugin {
 	
-	protected static final String MINECART_MANIA_PLUGIN_NAME = "Minecart Mania Core";
+	protected static final String MINECART_MANIA_PLUGIN_NAME = "MinecartManiaCore";
 	
-	private ConductorVehicleListener vehicleListener;
+	protected ConductorVehicleListener vehicleListener;
+	protected HashMap<Player,String> destinations;
 	
 	@Override
 	public void onEnable() {
@@ -22,7 +30,11 @@ public class ConductorPlugin extends JavaPlugin {
 		
 		if (isEnabled()) {
 			vehicleListener = new ConductorVehicleListener(this);
-			// TODO Register event handlers
+			destinations = new HashMap<Player,String>();
+			
+			PluginManager manager = getServer().getPluginManager();
+			manager.registerEvent(Event.Type.CUSTOM_EVENT, vehicleListener, Event.Priority.High, this);
+			
 			log("Version " + getDescription().getVersion() + " loaded.");
 		}
 	}
@@ -33,8 +45,48 @@ public class ConductorPlugin extends JavaPlugin {
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		return super.onCommand(sender, cmd, commandLabel, args);
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		if (command.getName().equalsIgnoreCase("to")) {
+			return onToCommand(sender, command, commandLabel, args);
+		}
+		
+		return false;
+	}
+	
+	protected boolean onToCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		if (!(sender instanceof Player)) {
+			return false;
+		}
+		
+		Player player = (Player)sender;
+		
+		if (args.length > 0) {
+			String destination = StringUtils.join(args, 0, " ");
+			// TODO Search through known destinations for possible matches
+			setDestinationFor(player, destination);
+			sendMessageTo(player, "You will disembark at " + destination + "!");
+		} else {
+			clearDestinationFor(player);
+			sendMessageTo(player, "You no longer have a destination.");
+		}
+		
+		return true;
+	}
+	
+	public void setDestinationFor(Player player, String destination) {
+		destinations.put(player, destination);
+	}
+	
+	public String getDestinationFor(Player player) {
+		return destinations.get(player);
+	}
+	
+	public void clearDestinationFor(Player player) {
+		destinations.remove(player);
+	}
+	
+	public void sendMessageTo(Player player, String message) {
+		player.sendMessage(ChatColor.GRAY + message);
 	}
 
 	protected Logger getLogger() {
--- src/com/raws/conductor/ConductorVehicleListener.java
+++ src/com/raws/conductor/ConductorVehicleListener.java
@@ -1,11 +1,89 @@
 package com.raws.conductor;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+
+import com.afforess.minecartmaniacore.MinecartManiaMinecart;
+import com.afforess.minecartmaniacore.event.MinecartActionEvent;
 import com.afforess.minecartmaniacore.event.MinecartManiaListener;
+import com.afforess.minecartmaniacore.utils.SignUtils;
+import com.afforess.minecartmaniacore.utils.StringUtils;
 
 public class ConductorVehicleListener extends MinecartManiaListener {
-	private final ConductorPlugin plugin;
+	protected static final Material STATION_BLOCK = Material.LOG;
+	
+	protected final ConductorPlugin conductor;
 	
 	public ConductorVehicleListener(ConductorPlugin plugin) {
-		this.plugin = plugin;
+		this.conductor = plugin;
+	}
+
+	@Override
+	public void onMinecartActionEvent(MinecartActionEvent event) {
+		if (!event.isActionTaken() && isMinecartAtStation(event)) {
+			onMinecartAtStation(event);
+		}
+	}
+	
+	protected void onMinecartAtStation(MinecartActionEvent event) {
+		MinecartManiaMinecart minecart = event.getMinecart();
+		
+		if (!(minecart.hasPlayerPassenger() && minecart.isOnRails())) {
+			return;
+		}
+		
+		Player player = minecart.getPlayerPassenger();
+		String destination = conductor.getDestinationFor(player);
+		
+		if (destination == null) {
+			return;
+		}
+		
+		// Replace whitespace in destination search query with a regex
+		// pattern matching anything, for a more lenient search.
+		String destinationPattern = destination.replaceAll("\\s+", ".*");
+		
+		ArrayList<Sign> signs = SignUtils.getAdjacentSignList(minecart, 2);
+		for (Sign sign : signs) {
+			String lines = StringUtils.join(sign.getLines(), 0, "\n");
+			String regex = "line(.*" + destinationPattern + ".*)";
+			Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+			Matcher matcher = pattern.matcher(lines);
+			
+			if (matcher.find()) {
+				minecart.stopCart();
+				minecart.kill(true);
+				
+				String stationName = matcher.group(1).trim().replaceAll("[\r\n]+", " ");
+				conductor.sendMessageTo(player, "You've arrived at " + stationName + "!");
+				conductor.clearDestinationFor(player);
+				
+				break;
+			}
+		}
+		
+		event.setActionTaken(true);	
+	}
+	
+	protected boolean isMinecartAtStation(MinecartManiaMinecart minecart) {
+		return minecart.getBlockIdBeneath() == STATION_BLOCK.getId();
+	}
+	
+	protected boolean isMinecartAtStation(MinecartActionEvent event) {
+		return isMinecartAtStation(event.getMinecart());
+	}
+	
+	protected void log(Level level, String message) {
+		conductor.log(level, message);
+	}
+	
+	protected void log(String message) {
+		conductor.log(message);
 	}
 }
