8e7ee968952a4514cacedcc4c2b396fdc57f476f
Ross Paffett <ross@rosspaffett.com>
2011-04-08 01:39:47 -0400

    Add documentation to ConductorPlugin

--- src/com/raws/conductor/ConductorPlugin.java
+++ src/com/raws/conductor/ConductorPlugin.java
@@ -61,11 +61,13 @@ public class ConductorPlugin extends JavaPlugin {
 		Player player = (Player)sender;
 		
 		if (args.length > 0) {
+			// Player would like to set her destination
 			String destination = StringUtils.join(args, 0, " ");
 			// TODO Search through known destinations for possible matches
 			setDestinationFor(player, destination);
 			sendMessageTo(player, "You will disembark at " + destination + "!");
 		} else {
+			// Player would like to clear her destination
 			clearDestinationFor(player);
 			sendMessageTo(player, "You no longer have a destination.");
 		}
@@ -73,18 +75,53 @@ public class ConductorPlugin extends JavaPlugin {
 		return true;
 	}
 	
+	/**
+	 * Set a player's Conductor destination.
+	 * 
+	 * The player's destination is a search query. Signs adjacent to stations will
+	 * be searched for a line matching the regular expression <code>/^.*line$/i</code>,
+	 * then searched for additional text matching the individual words of the
+	 * player's destination. So, for instance, a destination of <code>gaf</code>
+	 * would match a sign reading <code>Orange Line\nFort Gafley</code>.
+	 * 
+	 * @param player      player whose destination to set
+	 * @param destination string loosely matching the player's target destination
+	 */
 	public void setDestinationFor(Player player, String destination) {
 		destinations.put(player, destination);
 	}
 	
+	/**
+	 * Get a player's Conductor destination, if any.
+	 * 
+	 * See {@link #setDestinationFor(Player, String)} for a description of
+	 * player destinations.
+	 * 
+	 * @param player player whose destination to get
+	 * @return player's destination or <code>null</code>
+	 */
 	public String getDestinationFor(Player player) {
 		return destinations.get(player);
 	}
 	
+	/**
+	 * Remove a player's Conductor destination, if any.
+	 * 
+	 * See {@link #setDestinationFor(Player, String)} for a description of
+	 * player destinations.
+	 * 
+	 * @param player player whose destination to remove
+	 */
 	public void clearDestinationFor(Player player) {
 		destinations.remove(player);
 	}
 	
+	/**
+	 * Send a Conductor-branded message to a player.
+	 * 
+	 * @param player  player to whom to send the message
+	 * @param message message to send
+	 */
 	public void sendMessageTo(Player player, String message) {
 		player.sendMessage(ChatColor.GRAY + message);
 	}
